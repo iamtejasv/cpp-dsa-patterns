0b328f2a6ce0cb3ac11df6f610a3ee5893ef76d7
iamtejasv <tejasv0804@gmail.com>
2024-11-16 19:48:30 +0530

    add pattern 2

---
+++ pattern2.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern2(int n){
+    for(int i = 0; i < n; i++){ 
+        for(int j = 0; j <= i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int t; // t -> test case 
+    cin >> t;
+    for(int i = 0; i < t; i++){ 
+        int n;
+        cin >> n;
+        pattern2(n);
+    }
+    return 0;
+}

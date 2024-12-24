6bce000e5a93356671262ff6c735898043fc9aaa
iamtejasv <tejasv0804@gmail.com>
2024-12-24 11:59:07 +0530

    add pattern 22

---
+++ pattern22.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern22(int n)
+{
+     for(int i=0;i<2*n-1;i++){
+         for(int j=0;j<2*n-1;j++){
+             int top = i;
+             int bottom = j;
+             int right = (2*n - 2) - j;
+             int left = (2*n - 2) - i;
+             cout << (n- min(min(top,bottom), min(left,right))) << " ";
+         }
+         cout<<endl;
+     }
+}
+int main() {
+  int t;
+  cin >> t;
+  for(int i = 0; i < t; i++) {
+    int n;
+    cin >> n;
+    pattern22(n);
+  }
+}
